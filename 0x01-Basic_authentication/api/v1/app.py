158472305ef361dc3f96b3e076b1441baf7dc12f
esayyas <esayyasahmed1012@gmail.com>
2023-03-07 21:20:24 +0300

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,74 +13,53 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
 
-if AUTH_TYPE == "auth":
+if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+
+elif getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
+@app.before_request
+def beforeRequest() -> None:
+    """ Method to filter every request.
+    """
+    if auth is not None:
+        if auth.require_auth(path=request.path,
+                             excluded_paths=["/api/v1/status/",
+                                             "/api/v1/unauthorized/",
+                                             "/api/v1/forbidden/"]):
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """ Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Handle a unauthorized access
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+def RequestUnauthorized(error) -> str:
+    """ Request unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """Handle a forbidden resource
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+def RequestForbidden(error) -> str:
+    """ Request Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request() -> str:
-    """Execute before each request
-
-        Return:
-            String or nothing
-    """
-    if auth is None:
-        return
-
-    expath = ['/api/v1/status/',
-              '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
-
-    if not (auth.require_auth(request.path, expath)):
-        return
-
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
-    if (auth.current_user(request)) is None:
-        abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
